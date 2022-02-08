4b4a1a1bafda7c1a6e7cb8482c78bc73ec116427
Rafał Mikołajun <root@rmweb.pl>
2022-02-08 12:19:12 +0100

    fix Configuration

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -27,8 +27,8 @@ class Configuration implements ConfigurationInterface
      */
     public function getConfigTreeBuilder()
     {
-        $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root(self::ROOT_NAME);
+        $treeBuilder = new TreeBuilder(self::ROOT_NAME);
+        $rootNode = $treeBuilder->getRootNode();
 
         $rootNode
             ->children()
